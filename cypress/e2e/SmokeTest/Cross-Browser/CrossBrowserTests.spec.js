3f33c9275ae7562f82a2987d63c1f537b5c789f7
Pooja Hurry <pooja.hurry@unipro.io>
2023-03-07 12:17:44 +0400

    Test update text

---
+++ cypress/e2e/SmokeTest/Cross-Browser/CrossBrowserTests.spec.js
@@ -0,0 +1,59 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false;
+  });
+
+//https://www.unipro.io/
+it('load Home page',() => {
+    cy.visit('https://www.unipro.io/', {timeout: 40000});
+    cy.get('footer').scrollIntoView({ duration: 3000 })
+    cy.wait(10000)
+})
+ 
+//testing
+
+//https://www.unipro.io/services
+it('load services page',() => {
+  cy.visit('https://www.unipro.io/services', {timeout: 40000});
+  cy.get('footer').scrollIntoView({ duration: 3000 })
+  cy.wait(10000)
+})
+
+
+//https://www.unipro.io/how-we-work
+it('load how-we-work page',() => {
+  cy.visit('https://www.unipro.io/how-we-work', {timeout: 40000});
+  cy.get('footer').scrollIntoView({ duration: 3000 })
+  cy.wait(10000)
+})
+
+
+//https://www.unipro.io/our-work
+it('load our-work page',() => {
+  cy.visit('https://www.unipro.io/our-work', {timeout: 40000});
+  cy.get('footer').scrollIntoView({ duration: 3000 })
+  cy.wait(10000)
+})
+
+
+//https://insight.unipro.io/
+it('load insights page',() => {
+  cy.visit('https://insight.unipro.io/', {timeout: 40000});
+  cy.get('footer').scrollIntoView({ duration: 3000 })
+  cy.wait(10000)
+})
+
+
+//https://www.unipro.io/about
+it('load about page',() => {
+  cy.visit('https://www.unipro.io/about', {timeout: 40000});
+  cy.get('footer').scrollIntoView({ duration: 3000 })
+  cy.wait(10000)
+})
+
+
+//https://www.unipro.io/contact/
+it('load contact page',() => {
+  cy.visit('https://www.unipro.io/contact/', {timeout: 40000});
+  cy.get('footer').scrollIntoView({ duration: 3000 })
+  cy.wait(10000)
+})
